1442913c1ab8dbe90beac6e66b95c30910058645
mdsadiqueshakeel <mdsadiqueshakeel@gmail.com>
2025-09-25 23:03:21 +0530

    2 Questions 25/09/2025

--- JAVA/Arrays/Day_5.java
+++ JAVA/Arrays/Day_5.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 
 public class Day_5{
+    // Q1. Two Sum 
 
     public static int[] Two_sum(int[] arr, int target){
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -16,6 +17,61 @@ public class Day_5{
         return new int[]{};
     }
 
+    //Q2. Sort an Array of 0s, 1s and 2s (Better) -----> Brute was the merge sort <------
+    public static int[] Sort_ZOT_Better(int[] arr){
+        int count0 =0;
+        int count1 =0;
+        int count2 =0;
+        for(int i = 0; i < arr.length; i++){
+            if( arr[i] == 0){
+                count0++;
+            }
+            else if(arr[i]==1){
+                count1++;
+            }else{
+                count2++;
+            }
+        }
+
+        for(int i =0; i < count0; i++){
+            arr[i]= 0;
+        }
+        for(int i =count0 ; i < count0 + count1; i++){
+            arr[i]= 1;
+        }
+        for(int i =count0 + count1; i < arr.length; i++){
+            arr[i]= 2;
+        }
+
+        return arr;
+    }
+
+    //Q2. Sort an Array of 0s, 1s and 2s (Optimal) <<---- Dutch National Algorithm ---->>
+    public static int[] DNF_Algo(int[] arr){
+        int low = 0;
+        int mid = 0;
+        int high = arr.length -1;
+        while(mid < high){
+            if(arr[mid] == 0){
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+                mid++;
+            }
+            if(arr[mid] == 1){
+                mid++;
+            }
+            if(arr[mid] == 2){
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high --;
+            }
+        }
+        return arr;
+    }
+
     public static void print(int arr[]){
       for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i]+" ");
@@ -27,5 +83,10 @@ public class Day_5{
         int[] arr = {1,2,3,6,1,2,8,10};
         int target = 14;
         print(Two_sum(arr, target));
+
+        int[] zot_arr = {0,1,2,0,2,0,1,0,2,1,2,1,1};
+        print(Sort_ZOT_Better(zot_arr));   // TC- O(2N) SC- O(1)
+        print(DNF_Algo(zot_arr)); // TC- O(N) SC- O(1)
+
     }
 }
--- PYTHON/Arrays/Day_5.py
+++ PYTHON/Arrays/Day_5.py
@@ -1,5 +1,5 @@
 
-
+# Q1. Two Sum 
 def TwoSum(arr,k):
     map = {}
     for i in range(0, len(arr)):
@@ -10,6 +10,51 @@ def TwoSum(arr,k):
         map[a]= i
     return -1
 
+# Q2. Sort an Array of 0s, 1s and 2s (Better) -----> Brute was the merge sort <------
+def Sort_ZOT_Better(arr):
+    count0 = 0
+    count1 = 0
+    count2 = 0
+    for i in range(0,len(arr)):
+        if arr[i] == 0:
+            count0 += 1
+        elif arr[i] == 1:
+            count1 += 1
+        else: 
+            count2 += 1    
+    for i in range(0,count0):
+        arr[i] = 0
+    for i in range(count0, count0 + count1):
+        arr[i] = 1 
+    for i in range(count0 + count1 ,len(arr)):
+        arr[i] = 2
+
+    return arr
+
+# Q2. Sort an Array of 0s, 1s and 2s (Optimal) <<---- Dutch National Algorithm ---->>
+def DNF_Algo_ZOT(arr):
+    low = 0
+    mid = 0
+    high = len(arr)
+    while(mid < high):
+        if(arr[mid]==0):
+            arr[mid], arr[low] = arr[low],arr[mid]
+            low += 1
+            mid += 1
+        elif(arr[mid]==1):
+            mid += 1
+        else:
+            arr[mid],arr[high-1] = arr[high-1], arr[mid]
+            high -= 1    
+    return arr
+
+
+
 arr = [1,2,3,6,1,2,8,10]
+print(len(arr))
 target = 14
 print(TwoSum(arr,target))
+
+arr2 = [0,1,2,0,2,0,1,0,2,1,2,1,1]
+# print(Sort_ZOT_Better(arr2))
+print(DNF_Algo_ZOT(arr2))
