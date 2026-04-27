5dcbfd6a644b146814d1a1019ac0be437545885f
mdsadiqueshakeel <mdsadiqueshakeel@gmail.com>
2026-04-27 18:37:56 +0530

    46.38 LPA

--- Recursion/fn3.java
+++ Recursion/fn3.java
@@ -47,10 +47,45 @@ class fn3 {
         }
         
     }
+
+     static int fn(int arr[], int low, int high){
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while( i < j ){
+            while(arr[i] <= pivot && i <= high -1){
+                i++;
+            }
+            while(arr[j] > pivot && j >= low-1 ){
+                j--;
+            }  
+            if(i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        
+        return j;
+    }
+    
+    
+    static void quickSort(int arr[], int low, int high){
+        if(low < high){
+            int pivotIndex = fn(arr, low, high);
+            quickSort(arr, low, pivotIndex -1);
+            quickSort(arr, pivotIndex +1, high);
+        }
+    }
+    
     
     public static void main(String[] args) {
         int arr[] = {1,4,2,4,1,17,0,5,2,8}; //Q1
-        mergeSort(arr,0,arr.length-1);
+        // mergeSort(arr,0,arr.length-1);
+        quickSort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 }
